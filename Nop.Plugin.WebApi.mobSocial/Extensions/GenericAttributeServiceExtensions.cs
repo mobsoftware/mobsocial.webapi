32ea061aa49a51b2faf3f751b293c05ca83eeb3c
anshul <anshul@apexol.com>
2016-03-04 20:01:00 +0530

    Quickfix for avatar and cover uploads problem

---
+++ Nop.Plugin.WebApi.mobSocial/Extensions/GenericAttributeServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Nop.Core;
+using Nop.Core.Data;
+using Nop.Core.Domain.Common;
+using Nop.Core.Infrastructure;
+using Nop.Data;
+using Nop.Services.Common;
+
+namespace Nop.Plugin.WebApi.MobSocial.Extensions
+{
+    public static  class GenericAttributeServiceExtensions
+    {
+        public static GenericAttribute GetAttributeByKey(this IGenericAttributeService genericAttributeService, BaseEntity entity, string key, int storeId = 0)
+        {
+            var entityName = entity.GetUnproxiedEntityType().Name;
+            var ga = genericAttributeService.GetAttributesForEntity(entity.Id, entityName)
+                .FirstOrDefault(
+                    x =>
+                        x.Key == key && 
+                        x.StoreId == storeId);
+            if (ga != null) //weird but it works this way only
+                ga = genericAttributeService.GetAttributeById(ga.Id);
+            return ga;
+        }
+    }
+}
