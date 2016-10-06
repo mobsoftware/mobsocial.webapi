d969dcbba1e801da89b290c0b5b796dbe0b80beb
anshul <anshul@apexol.com>
2016-10-06 22:02:16 +0530

    Implemented ref parameter reading on customer registration event

--- Nop.Plugin.WebApi.mobSocial/EventConsumers/CustomerRegisteredEventConsumer.cs
+++ Nop.Plugin.WebApi.mobSocial/EventConsumers/CustomerRegisteredEventConsumer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Web;
+using Mob.Core.Extensions;
 using Nop.Core.Domain.Customers;
+using Nop.Plugin.WebApi.MobSocial.Domain;
 using Nop.Plugin.WebApi.MobSocial.Enums;
 using Nop.Plugin.WebApi.MobSocial.Events;
 using Nop.Plugin.WebApi.MobSocial.Services;
@@ -38,6 +41,36 @@ namespace Nop.Plugin.WebApi.MobSocial.EventConsumers
 
                 //publish the invitation accepted event to capture in other plugins
                 var inviter = allInviters.First(x => x.Id == invite.InviterUserId);
+                var invitationAcceptedEvent = new InvitationAcceptedEvent(inviter, customer);
+                _eventPublisher.Publish(invitationAcceptedEvent);
+            }
+
+            //in case this customer has registered with a different email than the one on which invitation was sent, it's better to check
+            //for ref attribute to see if there is the userid of the inviter
+            if (string.IsNullOrEmpty(HttpContext.Current.Request.QueryString["ref"]))
+                return;
+
+            var refId = HttpContext.Current.Request.QueryString["ref"];
+            var userId = refId.IsInteger() ? Convert.ToInt32(refId) : 0;
+            if (userId != 0)
+            {
+                var inviter = _customerService.GetCustomerById(userId);
+                if (inviter == null)
+                    return;
+                //we'll have to make a new invitation entry and assign it to the userId (inviter that is)
+                var invite = new Invitation()
+                {
+                    DateCreated = DateTime.UtcNow,
+                    AcceptedOn = DateTime.UtcNow,
+                    DateUpdated = DateTime.UtcNow,
+                    InviteeName = customer.GetFullName(),
+                    InviteeEmailAddress = customer.Email,
+                    InviteStatus = InviteStatus.Accepted,
+                    InviteeUserId = customer.Id,
+                    InviterUserId = userId
+                };
+                _invitationService.Insert(invite);
+                //publish event
                 var invitationAcceptedEvent = new InvitationAcceptedEvent(inviter, customer);
                 _eventPublisher.Publish(invitationAcceptedEvent);
             }
